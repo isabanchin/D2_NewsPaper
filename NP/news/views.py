c7229bb3c2b2c918b4bdd098b73e942e5706e86a
Igor <isabanchin@gmail.com>
2023-12-25 13:29:13 +0300

    Including VIEW and Bootstrap

--- NP/news/views.py
+++ NP/news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# позволяет выводить данные модели пользователя во view
+from django.views.generic import ListView
+from .models import Post, Category
+
+
+class NewsList(ListView):
+    model = Post                    # указываем модель объекты которой мы будем выводить
+    # указываем имя шаблона в котором будет лежать html с инструкциями для представление для пользователя
+    template_name = 'news/news.html'
+    # указываем имя списка в котором будут лежать все объекты для обращения к списку объектов через html-шаблон
+    context_object_name = 'news'
